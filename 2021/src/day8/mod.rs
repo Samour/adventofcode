e9fb95bbbea899f983784b05770b74b07387129b
Samour <aaron.burke.cpp@gmail.com>
2021-12-11 22:57:43 +1100

    Day 8 Enhance data structures & parsing logic

--- 2021/src/day8/mod.rs
+++ 2021/src/day8/mod.rs
@@ -19,7 +19,21 @@ impl DigitalOutput {
   }
 }
 
-fn parse_digital_output(raw_output: String) -> Option<DigitalOutput> {
+struct DisplayAnalysis {
+  sample_outputs: Vec<DigitalOutput>,
+  final_output: [DigitalOutput; 4],
+}
+
+impl DisplayAnalysis {
+  fn new(sample_outputs: Vec<DigitalOutput>, final_output: [DigitalOutput; 4]) -> DisplayAnalysis {
+    DisplayAnalysis {
+      sample_outputs,
+      final_output,
+    }
+  }
+}
+
+fn parse_digital_output(raw_output: &str) -> Option<DigitalOutput> {
   let raw_output = raw_output.trim();
   if raw_output.len() > 0 {
     Some(DigitalOutput::new(raw_output.chars().collect()))
@@ -28,21 +42,44 @@ fn parse_digital_output(raw_output: String) -> Option<DigitalOutput> {
   }
 }
 
-fn parse_all_output_digits(raw_data: String) -> Vec<DigitalOutput> {
+fn parse_display_analysis(raw_line: &str) -> Option<DisplayAnalysis> {
+  let line: Vec<&str> = raw_line.split("|").collect();
+  if line.len() != 2 {
+    return None;
+  }
+  let sample_outputs: Vec<DigitalOutput> = line[0]
+    .split(" ")
+    .map(parse_digital_output)
+    .filter(|d| d.is_some())
+    .map(|d| d.unwrap())
+    .collect();
+  let mut final_output: Vec<DigitalOutput> = line[1]
+    .split(" ")
+    .map(parse_digital_output)
+    .filter(|d| d.is_some())
+    .map(|d| d.unwrap())
+    .collect();
+  if final_output.len() != 4 {
+    return None;
+  }
+
+  let o4 = final_output.pop().unwrap();
+  let o3 = final_output.pop().unwrap();
+  let o2 = final_output.pop().unwrap();
+  let o1 = final_output.pop().unwrap();
+  Some(DisplayAnalysis::new(sample_outputs, [o1, o2, o3, o4]))
+}
+
+fn parse_displays(raw_data: &str) -> Vec<DisplayAnalysis> {
   raw_data
     .split("\n")
-    .map(|l| l.split("|").collect::<Vec<&str>>())
-    .filter(|l| l.len() > 1)
-    .map(|l| l[1])
-    .flat_map(|o| o.split(" "))
-    .map(String::from)
-    .map(parse_digital_output)
+    .map(parse_display_analysis)
     .filter(|d| d.is_some())
     .map(|d| d.unwrap())
     .collect()
 }
 
-fn count_matching_filter(digits: Vec<DigitalOutput>, config: Config) -> Result<(), String> {
+fn count_matching_filter(displays: Vec<DisplayAnalysis>, config: Config) -> Result<(), String> {
   let filter_counts = config.filter_by_segment_count;
   if filter_counts.is_none() {
     return Err(String::from(
@@ -50,8 +87,9 @@ fn count_matching_filter(digits: Vec<DigitalOutput>, config: Config) -> Result<(
     ));
   }
   let filter_counts = filter_counts.unwrap();
-  let count = digits
+  let count = displays
     .iter()
+    .flat_map(|d| d.final_output.iter())
     .filter(|d| filter_counts.contains(&d.segments.len()))
     .count();
   println!(
@@ -65,6 +103,6 @@ fn count_matching_filter(digits: Vec<DigitalOutput>, config: Config) -> Result<(
 pub fn main(factory: ContextFactory) -> Result<(), String> {
   let context: Context<Config> = factory.create()?;
   let raw_data = context.load_data(&context.config.data_file)?;
-  let digits = parse_all_output_digits(raw_data);
-  count_matching_filter(digits, context.config)
+  let displays = parse_displays(&raw_data);
+  count_matching_filter(displays, context.config)
 }
