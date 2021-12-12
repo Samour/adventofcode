da6552b479f83d51a334d81bf5d663e2a15381b3
Samour <aaron.burke.cpp@gmail.com>
2021-12-12 20:57:58 +1100

    Tidy up results output

--- 2021/src/test_harness.rs
+++ 2021/src/test_harness.rs
@@ -30,6 +30,83 @@ struct TestOutcome<'a> {
   time: u128,
 }
 
+struct TestResultAggregator<'a> {
+  print_immediate: bool,
+  show_errors: bool,
+  outcomes: Vec<TestOutcome<'a>>,
+}
+
+impl<'a> TestResultAggregator<'a> {
+  fn new(print_immediate: bool, show_errors: bool) -> TestResultAggregator<'a> {
+    TestResultAggregator {
+      print_immediate,
+      show_errors,
+      outcomes: Vec::new(),
+    }
+  }
+
+  fn print_results_header(&self) {
+    println!("{:<80}{:<15}{:>10}", "Test case", "Outcome", "Time (ms)");
+  }
+
+  fn print_test_result(&self, result: &TestOutcome) {
+    let outcome = match result.result {
+      Ok(_) => "Completed",
+      Err(_) => "Error",
+    };
+    println!(
+      "{:<80}{:<15}{:>20}",
+      result.config.name, outcome, result.time
+    );
+    if self.show_errors && result.result.is_err() {
+      println!("       {}", result.result.as_ref().unwrap_err());
+    }
+  }
+
+  fn print_aggregates(&self) {
+    let mut c_passed: i32 = 0;
+    let mut c_failed: i32 = 0;
+    let mut c_error: i32 = 0;
+    let mut total_time: u128 = 0;
+    for result in &self.outcomes {
+      match result.result {
+        Ok(_) => c_passed += 1,
+        Err(_) => c_error += 1,
+      }
+      total_time += result.time;
+    }
+
+    println!();
+    println!("Tests passed: {}", c_passed);
+    println!("Tests failed: {}", c_failed);
+    println!("Test errors: {}", c_error);
+    println!("Total execution time: {} ms", total_time);
+  }
+
+  fn initialise(&self) {
+    if self.print_immediate {
+      self.print_results_header();
+    }
+  }
+
+  fn emit_result(&mut self, outcome: TestOutcome<'a>) {
+    if self.print_immediate {
+      self.print_test_result(&outcome);
+    }
+    self.outcomes.push(outcome);
+  }
+
+  fn finalise(&self) {
+    if !self.print_immediate {
+      self.print_results_header();
+      for result in &self.outcomes {
+        self.print_test_result(result);
+      }
+    }
+    self.print_aggregates();
+  }
+}
+
 fn run_test<'a>(config_fname: String, config: &'a TestCase) -> TestOutcome<'a> {
   let start = Instant::now();
   let result = execute(config_fname, Writer::NoopWriter);
@@ -44,9 +121,17 @@ fn run_test<'a>(config_fname: String, config: &'a TestCase) -> TestOutcome<'a> {
 
 pub fn execute_tests(config_fname: String) -> Result<(), String> {
   let context: Context<TestSuite> = ContextFactory::new(config_fname).create()?;
-  let mut results: Vec<TestOutcome> = Vec::new();
+  let show_errors = context
+    .config
+    .config
+    .as_ref()
+    .map(|c| c.show_errors)
+    .flatten()
+    .unwrap_or(false);
+  let mut results: TestResultAggregator = TestResultAggregator::new(true, show_errors);
+  results.initialise();
   for suite in &context.config.tests {
-    results.push(match context.get_resource(&suite.config) {
+    results.emit_result(match context.get_resource(&suite.config) {
       Ok(suite_fname) => run_test(suite_fname, suite),
       Err(e) => TestOutcome {
         config: suite,
@@ -55,28 +140,7 @@ pub fn execute_tests(config_fname: String) -> Result<(), String> {
       },
     })
   }
-
-  // TODO some real formatting
-  let show_errors = context
-    .config
-    .config
-    .map(|c| c.show_errors)
-    .flatten()
-    .unwrap_or(false);
-  println!("Test case                  Outcome          Time (ms)");
-  for result in results {
-    let outcome = match result.result {
-      Ok(_) => "Completed",
-      Err(_) => "Error",
-    };
-    println!(
-      "{}        {}        {}",
-      result.config.name, outcome, result.time
-    );
-    if show_errors && result.result.is_err() {
-      println!("      {}", result.result.unwrap_err());
-    }
-  }
+  results.finalise();
 
   Ok(())
 }
