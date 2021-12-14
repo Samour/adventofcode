1d9fc12e187c3c8e40f5cddff7c8f5181bfee04b
Samour <aaron.burke.cpp@gmail.com>
2021-12-14 21:37:34 +1100

    Day 14 Part 2 Re-implement more efficiently so that solution can be found

--- 2021/src/day14/mod.rs
+++ 2021/src/day14/mod.rs
@@ -8,7 +8,7 @@ use crate::writer::Writer;
 struct Config {
   rules_file: String,
   steps: i32,
-  print_polymer: bool,
+  debug: Option<bool>,
 }
 
 struct InsertionRuleSet {
@@ -21,40 +21,81 @@ impl InsertionRuleSet {
   }
 }
 
+fn increment_count<K: Eq + std::hash::Hash>(map: &mut HashMap<K, i64>, key: K, increment: i64) {
+  let mut count = map.remove(&key).unwrap_or(0);
+  count += increment;
+  map.insert(key, count);
+}
+
 struct Polymer {
-  elements: LinkedList<char>,
+  first_char: char,
+  last_char: char,
+  elements: HashMap<(char, char), i64>,
 }
 
 impl Polymer {
-  fn new(elements: LinkedList<char>) -> Polymer {
-    Polymer { elements }
+  fn new(first_char: char, last_char: char, elements: HashMap<(char, char), i64>) -> Polymer {
+    Polymer {
+      first_char,
+      last_char,
+      elements,
+    }
   }
 
   fn apply_rules(&self, rule_set: &InsertionRuleSet) -> Polymer {
-    let mut new_elements: LinkedList<char> = LinkedList::new();
-    let mut last_char: Option<char> = None;
-    for &c in &self.elements {
-      match last_char {
-        Some(lc) => match rule_set.rules.get(&(lc, c)) {
-          Some(&insert) => new_elements.push_back(insert),
-          None => {}
-        },
-        None => {}
+    let mut elements: HashMap<(char, char), i64> = HashMap::new();
+    for (&pair, &count) in &self.elements {
+      match rule_set.rules.get(&pair) {
+        Some(&insert) => {
+          increment_count(&mut elements, (pair.0, insert), count);
+          increment_count(&mut elements, (insert, pair.1), count);
+        }
+        None => increment_count(&mut elements, pair, count),
       }
-      new_elements.push_back(c);
-      last_char = Some(c);
     }
 
-    Polymer::new(new_elements)
+    Polymer::new(self.first_char, self.last_char, elements)
   }
 
-  fn render(&self) -> String {
-    self.elements.iter().collect()
+  fn build_char_map(&self) -> HashMap<char, i64> {
+    let mut result: HashMap<char, i64> = HashMap::new();
+    let mut is_first: bool = true;
+    for (pair, &count) in &self.elements {
+      if is_first {}
+      increment_count(&mut result, pair.0, count);
+      increment_count(&mut result, pair.1, count);
+      is_first = false;
+    }
+    increment_count(&mut result, self.first_char, 1);
+    increment_count(&mut result, self.last_char, 1);
+
+    result.into_iter().map(|(k, v)| (k, v / 2)).collect()
+  }
+
+  fn explain_elements(&self, writer: &Writer) {
+    writer.write(|| format!("By pairs:"));
+    for (pair, count) in &self.elements {
+      writer.write(|| format!("({}, {}) -> {}", pair.0, pair.1, count));
+    }
+    writer.write(|| format!("By character:"));
+    for (c, count) in self.build_char_map() {
+      writer.write(|| format!("{} -> {}", c, count));
+    }
   }
 }
 
-fn parse_polymer(raw_poly: &str) -> Polymer {
-  Polymer::new(raw_poly.trim().chars().collect())
+fn parse_polymer(raw_poly: &str) -> Option<Polymer> {
+  let mut elements: HashMap<(char, char), i64> = HashMap::new();
+  let mut first_c: Option<char> = None;
+  let mut last_c: Option<char> = None;
+  for c in raw_poly.trim().chars() {
+    match last_c {
+      Some(lc) => increment_count(&mut elements, (lc, c), 1),
+      None => first_c = Some(c),
+    }
+    last_c = Some(c);
+  }
+  Some(Polymer::new(first_c?, last_c?, elements))
 }
 
 fn parse_rule(raw_rule: &str) -> Option<((char, char), char)> {
@@ -77,7 +118,7 @@ fn parse(raw_rules: String) -> Result<(Polymer, InsertionRuleSet), String> {
   let mut line_no = 0;
   for line in raw_rules.split("\n") {
     match line_no {
-      0 => polymer = Some(parse_polymer(line)),
+      0 => polymer = parse_polymer(line),
       1 => {}
       _ => match parse_rule(line) {
         Some((pattern, insert)) => {
@@ -100,23 +141,20 @@ fn apply_steps(
   rule_set: InsertionRuleSet,
   config: Config,
   writer: Writer,
-) -> Result<i32, String> {
+) -> Result<i64, String> {
+  let debug = config.debug.unwrap_or(false);
   for i in 0..config.steps {
     polymer = polymer.apply_rules(&rule_set);
-    if config.print_polymer {
-      writer.write(|| format!("Polymer: {}", polymer.render()));
+    if debug {
+      writer.write(|| format!("After step {}", i + 1));
+      polymer.explain_elements(&writer);
+      writer.write(|| format!(""));
     }
   }
 
-  let mut total_length: i32 = 0;
-  let mut char_count: HashMap<char, i32> = HashMap::new();
-  for c in polymer.elements {
-    total_length += 1;
-    let mut count = char_count.remove(&c).unwrap_or(0);
-    count += 1;
-    char_count.insert(c, count);
-  }
-  let mut counts: Vec<i32> = char_count.values().cloned().collect();
+  let mut counts: Vec<i64> = polymer.build_char_map().values().cloned().collect();
+  let total_length: i64 = counts.iter().sum();
+  // let total_length: i32 = polymer.elements.values().sum();
   counts.sort();
   let c_min = counts
     .get(0)
