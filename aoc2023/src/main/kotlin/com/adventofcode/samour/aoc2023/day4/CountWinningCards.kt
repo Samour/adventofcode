e2a1665f504166bee0fbe28e7d1007d05c7fed9d
Aaron Burke <aaronb@canva.com>
2023-12-04 16:24:03 +1000

    Solve Day 4 Part 2 using memoization

--- aoc2023/src/main/kotlin/com/adventofcode/samour/aoc2023/day4/CountWinningCards.kt
+++ aoc2023/src/main/kotlin/com/adventofcode/samour/aoc2023/day4/CountWinningCards.kt
@@ -9,15 +9,33 @@ fun countWinningCards(fname: String) = readResource("day4/$fname").use { file ->
 fun countCascadedCopies(fname: String) = readResource("day4/$fname").use { file ->
     val allCards = file.parseScratchCards()
 
-    val cardCopies = (1..allCards.size).associateWith { 1 }.toMutableMap()
-    fun updateCopyCount(cardIdx: Int) {
+    val copiesToCreateCache = mutableMapOf<Int, Map<Int, Int>>()
+    fun copiesToCreate(cardIdx: Int): Map<Int, Int> {
+        println("copiesToCreate($cardIdx)")
+        copiesToCreateCache[cardIdx]?.let { return it }
+
+        val copies = mutableMapOf<Int, Int>()
+        if (cardIdx == allCards.size + 1 || allCards[cardIdx - 1].winningCount == 0) {
+            copiesToCreateCache[cardIdx] = copies
+            return copies
+        }
+
         (1..allCards[cardIdx - 1].winningCount).forEach {
-            val nextIdx = cardIdx + it
-            cardCopies[nextIdx] = cardCopies[nextIdx]!! + 1
-            updateCopyCount(nextIdx)
+            copies[cardIdx + it] = copies.getOrDefault(cardIdx + it, 0) + 1
+            copiesToCreate(cardIdx + it).forEach { (copyIdx, copyCount) ->
+                copies[copyIdx] = copies.getOrDefault(copyIdx, 0) + copyCount
+            }
+        }
+
+        copiesToCreateCache[cardIdx] = copies
+        return copies
+    }
+
+    val cardCopies = (1..allCards.size).associateWith { 1 }.toMutableMap()
+    cardCopies.keys.forEach { cardIdx ->
+        copiesToCreate(cardIdx).forEach { (copyIdx, copyCount) ->
+            cardCopies[copyIdx] = cardCopies[copyIdx]!! + copyCount
         }
     }
-    cardCopies.keys.forEach { updateCopyCount(it) }
-
     cardCopies
 }
