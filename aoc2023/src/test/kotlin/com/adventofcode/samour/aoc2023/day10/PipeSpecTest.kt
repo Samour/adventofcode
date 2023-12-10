2c06af620faf46f3d619460228f42abe95bb2104
Aaron Burke <aaronb@canva.com>
2023-12-10 17:25:09 +1000

    Add test cases for pipe parsing

--- aoc2023/src/test/kotlin/com/adventofcode/samour/aoc2023/day10/PipeSpecTest.kt
+++ aoc2023/src/test/kotlin/com/adventofcode/samour/aoc2023/day10/PipeSpecTest.kt
@@ -7,10 +7,20 @@ import org.junit.jupiter.api.Test
 class PipeSpecTest {
 
     @Test
-    fun `Should filter out unexpected sections`() {
-        val spec = readResource("day10/sample.txt").use { it.parsePipeSpec() }
-        val withoutLoops = spec.withoutLoops()
+    fun `Should filter out unexpected sections in sample dataset`() {
+        val result = readResource("day10/sample.txt").use { it.parsePipeSpec() }
+            .withoutLoops()
 
-        assertThat(withoutLoops).isNull()
+        val expectedMap = readResource("day10/sample_simplified.txt").readText()
+        assertThat(result.debugRender()).isEqualTo(expectedMap)
+    }
+
+    @Test
+    fun `Should filter out unexpected sections in second sample dataset`() {
+        val result = readResource("day10/sample2.txt").use { it.parsePipeSpec() }
+            .withoutLoops()
+
+        val expectedMap = readResource("day10/sample2_simplified.txt").readText()
+        assertThat(result.debugRender()).isEqualTo(expectedMap)
     }
 }
